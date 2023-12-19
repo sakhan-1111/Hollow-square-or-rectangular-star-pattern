c76f01a4e5f8ea46c934ec62492a7f429808bce2
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 16:26:30 -0600

    Create Hollow square or rectangular star pattern.c

---
+++ Hollow square or rectangular star pattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, j, r, c ;
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    for(i=1; i<=r; i++)
+    {
+        for(j=1; j<=c; j++)
+        {
+            if(i==1 || i==r || j==1 || j==c)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
